0b1c17803876b32f35938578ebc55c743f4d9889
DanyloKopytko <leonelluson@gmail.com>
2019-10-02 10:07:26 +0300

    Yet another test

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 
 const makeDir = require('./makeDir');
+const makeInfo = require('./makeInfo');
+const copyImgIn = require('./copyImgIn');
+
 makeDir('jscx-1800','Danylo Kopytko');
 makeDir('jscx-2000','Random User');
 
-const makeInfo = require('./makeInfo');
 makeInfo('jscx-1800','Danylo Kopytko', 'Name: Danylo Kopytko, \n Age: 18 \n Skills: HTML, CSS, JS');
 makeInfo('jscx-2000','Random User', 'Name: Random Userovich, \n Age: 100 \n Skills: HTML, CSS, JS');
 
-const copyImgIn = require('./copyImgIn');
 copyImgIn('jscx-1800','Danylo Kopytko', 'WJ-skAHWO8M');
 copyImgIn('jscx-2000','Random User', 'WJ-skAHWO8M');
 
